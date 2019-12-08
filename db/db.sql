fb25347c5b2181fc227047aad1ad1288ad8c05dd
aaronkaplan <aaron@lo-res.org>
2019-12-08 23:31:23 +0100

    minor comment

--- db/db.sql
+++ db/db.sql
@@ -3,7 +3,7 @@ CREATE DATABASE bincache;
 \c bincache
 
 CREATE TABLE "cache" (
-    filename varchar(500), 
+    filename varchar(500),      -- file name on disk
     md5 varchar(33), 
     sha1 varchar(42), 
     sha256 varchar(65) primary key, 

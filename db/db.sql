edc26debfe04064936efcdd6ec65120cf8f8d6a2
aaronkaplan <aaron@lo-res.org>
2019-12-09 11:19:03 +0100

    Improve data model

--- db/db.sql
+++ db/db.sql
@@ -4,7 +4,7 @@ CREATE DATABASE bincache;
 
 CREATE TABLE "cache" (
     filename varchar(500),              -- file name of the binary 
-    path_on_disk varchar(500),          -- the file name we gave it on our disk
+    path_on_disk varchar(1000),          -- the file name we gave it on our disk
     md5 varchar(33), 
     sha1 varchar(42), 
     sha256 varchar(65) primary key, 
@@ -15,4 +15,7 @@ CREATE TABLE "cache" (
 
 
 CREATE index idx_cache_sha256 on "cache"(sha256);
+CREATE index idx_cache_sha1 on "cache"(sha1);
+CREATE index idx_cache_md5 on "cache"(md5);
+CREATE index idx_cache_filename on "cache"(filename);
 

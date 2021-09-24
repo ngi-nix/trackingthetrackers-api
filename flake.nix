0ba519a43fa47a447175f6e8b791c1ab748e5d4d
Mate Hajzso <ajz@lambd.ax>
2021-09-25 00:11:05 +0200

    removes *-darwin systems from build

--- flake.nix
+++ flake.nix
@@ -8,8 +8,16 @@
   };
 
   outputs = { self, nixpkgs, flake-utils, mach-nix-src }:
-    flake-utils.lib.eachDefaultSystem
-      (
+    let
+      lib = nixpkgs.lib;
+      defaultSystems = flake-utils.lib.defaultSystems;
+      buildSystems = lib.lists.subtractLists
+        [ "aarch64-darwin" "x86_64-darwin" ] defaultSystems;
+
+      #buildSystems = lib.lists.remove "aarch64-darwin"
+      #  (lib.lists.remove "x86_64-darwin" defaultSystems);
+    in
+      flake-utils.lib.eachSystem buildSystems (
         system:
           let
             pkgs = nixpkgs.legacyPackages.${system};
@@ -32,97 +40,97 @@
               };
             }
       ) // {
-      nixosModules = {
-        ttapi-db = (
-          { config, lib, pkgs, ... }:
-            let
-              db_user = "bincache";
-              db_name = "bincache";
-              cfg = config.services.ttapi-db;
-            in
-              {
-                options = {
-                  services.ttapi-db = {
-                    enable = lib.mkOption {
-                      default = true;
-                      type = lib.types.bool;
-                      description = ''
-                        Initialize and enable the Tracking the Trackers API database.
-                      '';
+        nixosModules = {
+          ttapi-db = (
+            { config, lib, pkgs, ... }:
+              let
+                db_user = "bincache";
+                db_name = "bincache";
+                cfg = config.services.ttapi-db;
+              in
+                {
+                  options = {
+                    services.ttapi-db = {
+                      enable = lib.mkOption {
+                        default = true;
+                        type = lib.types.bool;
+                        description = ''
+                          Initialize and enable the Tracking the Trackers API database.
+                        '';
+                      };
                     };
                   };
+
+                  config = {
+                    services.postgresql = {
+                      enable = lib.mkIf cfg.enable true;
+                      enableTCPIP = true;
+                      # Make sure we can auth from outside of vm..
+                      authentication = lib.mkForce ''
+                        local  all  all                         trust
+                        host   all  ${db_user}  10.0.0.0/8      trust
+                        host   all  ${db_user}  172.16.0.0/12   trust
+                        host   all  ${db_user}  192.168.0.0/16  trust
+                      '';
+                      ensureDatabases = [ "${db_name}" ];
+                      ensureUsers = [
+                        {
+                          name = "${db_user}";
+                          ensurePermissions = {
+                            "DATABASE ${db_name}" = "ALL PRIVILEGES";
+                            "ALL TABLES IN SCHEMA public" = "ALL PRIVILEGES";
+                          };
+                        }
+                      ];
+                      # Initialize table and indexes from project's db.sql
+                      initialScript = pkgs.writeTextFile {
+                        name = "initialScript.sql";
+                        text = (builtins.readFile ./db/db.sql); #+ ''
+                      };
+                    };
+                  };
+                }
+          );
+        };
+        nixosConfigurations.test-vm = nixpkgs.lib.nixosSystem {
+          # First build the VM:
+          #  $ nixos-rebuild build-vm --flake .#test-vm
+          # Then remove old images if any, set port forwards and run VM:
+          #  $ rm ttapi-db.qcow2; env QEMU_NET_OPTS=hostfwd=tcp:2221-:22,hostfwd=tcp::5433-:5432 result/bin/run-ttapi-db-vm
+          system = "x86_64-linux";
+          modules = [
+            (
+              { modulesPath, ... }: {
+                imports = [ (modulesPath + "/virtualisation/qemu-vm.nix") ];
+              }
+            )
+            self.nixosModules.ttapi-db
+            (
+              { config, lib, pkgs, ... }: {
+                system.configurationRevision = self.rev or "dirty";
+
+                virtualisation = {
+                  graphics = false;
                 };
 
-                config = {
-                  services.postgresql = {
-                    enable = lib.mkIf cfg.enable true;
-                    enableTCPIP = true;
-                    # Make sure we can auth from outside of vm..
-                    authentication = lib.mkForce ''
-                      local  all  all                         trust
-                      host   all  ${db_user}  10.0.0.0/8      trust
-                      host   all  ${db_user}  172.16.0.0/12   trust
-                      host   all  ${db_user}  192.168.0.0/16  trust
-                    '';
-                    ensureDatabases = [ "${db_name}" ];
-                    ensureUsers = [
-                      {
-                        name = "${db_user}";
-                        ensurePermissions = {
-                          "DATABASE ${db_name}" = "ALL PRIVILEGES";
-                          "ALL TABLES IN SCHEMA public" = "ALL PRIVILEGES";
-                        };
-                      }
-                    ];
-                    # Initialize table and indexes from project's db.sql
-                    initialScript = pkgs.writeTextFile {
-                      name = "initialScript.sql";
-                      text = (builtins.readFile ./db/db.sql); #+ ''
-                    };
-                  };
+                services = {
+                  getty.autologinUser = "root";
+                  openssh.enable = true;
+                  openssh.permitRootLogin = "yes";
+                };
+
+                networking = {
+                  hostName = "ttapi-db";
+                  firewall.allowedTCPPorts = [ 5432 22 ];
+                };
+
+                users = {
+                  extraUsers.root.password = "";
+                  mutableUsers = false;
                 };
               }
-        );
+            )
+          ];
+        };
       };
-      nixosConfigurations.test-vm = nixpkgs.lib.nixosSystem {
-        # First build the VM:
-        #  $ nixos-rebuild build-vm --flake .#test-vm
-        # Then remove old images if any, set port forwards and run VM:
-        #  $ rm ttapi-db.qcow2; env QEMU_NET_OPTS=hostfwd=tcp:2221-:22,hostfwd=tcp::5433-:5432 result/bin/run-ttapi-db-vm
-        system = "x86_64-linux";
-        modules = [
-          (
-            { modulesPath, ... }: {
-              imports = [ (modulesPath + "/virtualisation/qemu-vm.nix") ];
-            }
-          )
-          self.nixosModules.ttapi-db
-          (
-            { config, lib, pkgs, ... }: {
-              system.configurationRevision = self.rev or "dirty";
-
-              virtualisation = {
-                graphics = false;
-              };
-
-              services = {
-                getty.autologinUser = "root";
-                openssh.enable = true;
-                openssh.permitRootLogin = "yes";
-              };
-
-              networking = {
-                hostName = "ttapi-db";
-                firewall.allowedTCPPorts = [ 5432 22 ];
-              };
-
-              users = {
-                extraUsers.root.password = "";
-                mutableUsers = false;
-              };
-            }
-          )
-        ];
-      };
-    };
 }
